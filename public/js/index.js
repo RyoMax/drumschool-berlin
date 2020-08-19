2a6fdc0df400d40570c66599acd12d6a9adc9964
Maximilian Faust <BsHr_Faust@web.de>
2020-08-19 20:42:35 +0200

    added location section with interactive map plus various fixers v1

--- public/js/index.js
+++ public/js/index.js
@@ -39,6 +39,11 @@ $( document ).ready(function() {
         $(section + " .headliner").height(hHeight);
     }
 
+    function mapResizer(){
+        var mapWidth = $(".locations #map").width();
+        $(".locations #map").height(mapWidth);
+    }
+
     function getStartedResizer(){
         var imgHeight = $(".get-started img").height();
         var contactHeight = $(".get-started .get-contact").height();
@@ -62,12 +67,32 @@ $( document ).ready(function() {
         $(".news .soon").height(img);
     }
 
+    function initMap() {
+        // The location of Uluru
+        var uluru = {lat: -25.344, lng: 131.036};
+        // The map, centered at Uluru
+        var map = new google.maps.Map(
+            document.getElementById('map'), {zoom: 4, center: uluru});
+        // The marker, positioned at Uluru
+        var marker = new google.maps.Marker({position: uluru, map: map});
+    }
+
+    jQuery.expr.filters.offscreen = function(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+                 (rect.x + rect.width) < 0 
+                   || (rect.y + rect.height) < 0
+                   || (rect.x > window.innerWidth || rect.y > window.innerHeight)
+               );
+      };
+
     var sections = [".service", ".locations", ".teacher", ".news"];
     var timer;
 
     //welcomeResizer();
     
     getStartedResizer();
+    mapResizer();
     sections.forEach(function(e) {
         console.log("Beim Laden der Seite wurde die Höhe angepasst");
         headerResizer(e);
@@ -80,6 +105,7 @@ $( document ).ready(function() {
             console.log("Beim Laden der Seite wurde die Höhe angepasst");
             headerResizer(e);
         });
+        mapResizer();
         getStartedResizer();
         newsResizer();
     })
@@ -87,6 +113,7 @@ $( document ).ready(function() {
     $( window ).resize(function() {
         //console.log("Bildschirm Größe hat sich verändert!");
         //welcomeResizer();
+        mapResizer();
         getStartedResizer();
         newsResizer();
         sections.forEach(function(e) {
@@ -98,8 +125,137 @@ $( document ).ready(function() {
     $( window ).scroll(function(){
         window.clearTimeout(timer);
         fadeScroll();
-        timer = window.setTimeout(function(){ fadeScrollOut(); }, 500);
+        var offScreen = $("footer").is(":offscreen");
+        if(offScreen == true){
+            timer = window.setTimeout(function(){ fadeScrollOut(); }, 500);
+        }
     })
     
     
 });
+//google maps api
+function initMap() {
+    // The location of Friedrichshain/Lichtenberg location
+    var defaultLoc = {lat: 52.5095829, lng: 13.4902017};
+    // The map, centered at Friedrichshain/Lichtenberg
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 13, center: defaultLoc});
+    // The marker, positioned at Friedrichshain/Lichtenberg
+    var marker = new google.maps.Marker({position: defaultLoc, map: map});
+    
+    //changes the position of the marker depending on the clients choice
+    function changeMarkerPosition(marker, location) {
+        var latlng = new google.maps.LatLng(location.lat, location.lng);
+        //console.log("the new set latitude is " + location.lat);
+        marker.setPosition(latlng);
+        //marker.setLabel("Adresse")
+    }
+
+    function changeMapPosition(map, location) {
+        var latlng = new google.maps.LatLng(location.lat, location.lng);
+        map.setCenter(latlng);
+    }
+
+    /*function tempelhof(marker) {
+        var latlng = new google.maps.LatLng(52.466949, 13.392963);
+        marker.setPosition(latlng);
+    }*/
+
+    var locations = [ 
+        //These are the Coordinates for the locations in the districts:
+
+        //Friedrichshain/Lichtenberg 
+        {
+            loc: "fLichtenberg",
+            lat: 52.5095829, 
+            lng: 13.4902017
+        },
+        //Tempelhof
+        { 
+            loc: "tempelhof",
+            lat: 52.466949, 
+            lng: 13.392963
+        },
+        //Friedrichshain/Kreuzberg
+        {
+            loc: "fKreuzberg",
+            lat: 52.5046279,
+            lng: 13.4511253
+        },
+        //Mitte
+        {
+            loc: "mitte",
+            lat: 52.53611,
+            lng: 13.39728
+        },
+        //Wedding
+        {
+            loc: "wedding",
+            lat: 52.555476,
+            lng: 13.374834
+        },
+        //Pankow
+        {
+            loc: "pankow",
+            lat: 52.5634979,
+            lng: 13.409052
+        }
+    ];
+    //By clicking on the district names beneath/under the Map,
+    //The marker will change its position to the choosen location
+    $(".locations .location ").click(function(e){
+        console.log(e);
+        //console.log(e.target.id);
+        var currentLoc = ""
+        var newLoc = e.target.id;
+        //checks the choosen district/location
+        for (var i = 0; i < locations.length; i++) {
+            currentLoc = locations[i].loc;
+            //console.log(currentLoc)
+            if (currentLoc == newLoc){
+                newLoc = locations[i];
+            };
+            
+        };
+        //console.log("loop is over and the newLoc.lat  equals: " + newLoc.lat);
+        
+        
+        changeMarkerPosition(marker, newLoc);
+        changeMapPosition(map, newLoc)
+    });
+}
+/*document.querySelector(".locations .location").addEventListener("click", function(){
+    //changeMarkerPosition(marker);
+    console.log("CLICK!");
+})
+$(".locations .location").click(function(){
+    console.log("CLICK!!");
+});
+
+var fLichtenberg ={
+    lat: 52.5095829,
+    lng: 13.4902017
+};
+var tempelhof ={
+    lat: 52.466949,
+    lng: 13.392963
+};
+var fKreuzberg ={
+    lat: 52.5046279,
+    lng: 13.4511253
+};
+var mitte ={
+    lat: 52.53611,
+    lng: 13.39728
+};
+var wedding ={
+    lat: 52.555476,
+    lng: 13.374834
+};
+var pankow ={
+    lat: 52.5634979,
+    lng: 13.409052
+};*/
+
+
+//Maps API key:AIzaSyBfXJ_TeZHWtDMEpGuz3mMwuDcfXcRAXAM
